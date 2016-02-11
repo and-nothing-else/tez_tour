2b38c9b527d6c1b09fa02eef633d633d72d1349d
And Nothing Else <and.nothing.else@ya.ru>
2016-02-12 01:00:29 +0300

    меню скроллит к нужной секции

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,35 +3,60 @@ import $ from 'jquery'
 
 class App {
     constructor() {
+        this._initElements();
+        this.scrollDuration = 3000;
+
         this._initHeader();
         this._initMainMenu();
+        this._initScrollNav();
+    }
+
+    _initElements() {
+        this.$header = $(".page_header");
+        this.headerHeight = this.$header.outerHeight();
+        this.headerDefaultPos = this.$header.offset().top;
+
+        this.$mainMenu = $(".main_menu");
+        this.$mainMenuLink = this.$mainMenu.find(".main_menu__link");
+        this.$mainMenuTrigger = this.$mainMenu.find(".trigger");
     }
 
     _initHeader() {
-        const $header = $(".page_header"),
-            headerDefaultPos = $header.offset().top;
         let headerFixed = false;
-        function setMainMenuPosition() {
-            var scrollTop = $(window).scrollTop();
-            if(scrollTop >= headerDefaultPos) {
+        $(window).scroll(() => {
+            if($(window).scrollTop() >= this.headerDefaultPos) {
                 if(!headerFixed){
-                    $header.addClass("fixed");
+                    this.$header.addClass("fixed");
                     headerFixed = true;
                 }
             } else {
                 if(headerFixed){
-                    $header.removeClass("fixed");
+                    this.$header.removeClass("fixed");
                     headerFixed = false;
                 }
             }
-        }
-        $(window).scroll(setMainMenuPosition);
+        });
     }
 
     _initMainMenu() {
-        const $mainMenu = $(".main_menu"),
-            $mainMenuTrigger = $mainMenu.find(".trigger");
-        $mainMenuTrigger.on("click", () => $mainMenu.toggleClass("open"));
+        let self = this;
+        this.$mainMenuTrigger.on("click", () => this.$mainMenu.toggleClass("open"));
+        this.$mainMenuLink.on("click", function(){
+            self.scroll2Section($(this).attr("href").replace("#", ""));
+        });
+    }
+
+    _initScrollNav() {
+
+    }
+
+    scroll2Section(sectionID){
+        let $section = $("#section_" + sectionID),
+            scrollPosition = $section.offset().top - this.headerHeight;
+        $("html,body").animate({
+            scrollTop: scrollPosition,
+            duration: this.scrollDuration
+        });
     }
 }
 
