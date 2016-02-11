8a1ec5d5d4ea3967bc61416e672b8a92068781ef
And Nothing Else <and.nothing.else@ya.ru>
2016-02-12 00:15:09 +0300

    меню прибивается к верху

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,7 +3,29 @@ import $ from 'jquery'
 
 class App {
     constructor() {
+        this._initHeader();
         this._initMainMenu();
+    }
+
+    _initHeader() {
+        const $header = $(".page_header"),
+            headerDefaultPos = $header.offset().top;
+        let headerFixed = false;
+        function setMainMenuPosition() {
+            var scrollTop = $(window).scrollTop();
+            if(scrollTop >= headerDefaultPos) {
+                if(!headerFixed){
+                    $header.addClass("fixed");
+                    headerFixed = true;
+                }
+            } else {
+                if(headerFixed){
+                    $header.removeClass("fixed");
+                    headerFixed = false;
+                }
+            }
+        }
+        $(window).scroll(setMainMenuPosition);
     }
 
     _initMainMenu() {
